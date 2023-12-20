7d6b0e0ae8779aa32828e265dccbbffb90245667
herve <hdjomo1@gmail.com>
2023-12-20 13:38:20 +0100

    added some view

--- devoir/src/app/app.component.ts
+++ devoir/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TaskListComponent } from "./task-list/task-list.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, TaskListComponent]
 })
 export class AppComponent {
   title = 'devoir';
