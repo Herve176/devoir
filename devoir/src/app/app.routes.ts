0f58959e42c3475e00cf86c740cbc4c4998ca20f
herve <hdjomo1@gmail.com>
2023-12-22 15:08:45 +0100

    configure the rooting

--- devoir/src/app/app.routes.ts
+++ devoir/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:'',component:TaskListComponent},
+{path:'addtask',component:AddTaskComponent}
+];
